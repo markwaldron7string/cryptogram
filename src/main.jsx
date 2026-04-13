586739b662cadac464b5d434bbfae3baa1284b18
markwaldron7string <mark.waldron7string@gmail.com>
2026-04-13 08:08:49 -0600

    Fix Vercel base path

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cryptogram">
+    <HashRouter basename="/cryptogram">
       <CoinContextProvider>
         <App />
       </CoinContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
