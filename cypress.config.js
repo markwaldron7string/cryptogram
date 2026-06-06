611fcf6423e27e8ee7056b34c1738d2d8509d721
markwaldron7string <mark.waldron7string@gmail.com>
2026-06-05 20:39:57 -0600

    Use preview port 4173 for Cypress baseUrl in CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
     video: false,
